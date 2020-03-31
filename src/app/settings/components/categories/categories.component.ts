2ffc41d3351617c2bd71b7c7359089675f842a47
EugeniaPais <mariaeugeniapais@gmail.com>
2020-03-31 19:18:56 -0300

    Add container and presentational component

--- src/app/settings/components/categories/categories.component.ts
+++ src/app/settings/components/categories/categories.component.ts
@@ -1,59 +1,15 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { CashflowCategory } from "../../models/cashflow-category";
-import { SettingsFacade } from "../../settings.facade";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-categories",
-  templateUrl: "./categories.component.html",
-  styleUrls: ["./categories.component.css"]
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-  @Input() cashflowCategories$: CashflowCategory[];
-  newCategory: CashflowCategory = new CashflowCategory();
-  isUpdating$: Observable<boolean>;
-  showUpdate: boolean;
-  categories$: Observable<CashflowCategory[]>;
-  form: FormGroup;
-  constructor(
-    private settingsFacade: SettingsFacade,
-    private formBuilder: FormBuilder
-  ) {
-    this.isUpdating$ = settingsFacade.isUpdating$();
-  }
+
+  constructor() { }
 
   ngOnInit() {
-    this.initForm();
-    this.isUpdating$.subscribe(val => {
-      this.showUpdate = val;
-    });
-
-    this.settingsFacade.loadCashflowCategories().subscribe(val => {
-      this.categories$ = this.settingsFacade.getCashflowCategories$();
-    });
   }
 
-  addCategory(category: CashflowCategory) {
-    this.settingsFacade.addCashflowCategory(category);
-  }
-
-  updateCategory(category: CashflowCategory) {
-    this.settingsFacade.updateCashflowCategory(category);
-  }
-
-  initForm() {
-    this.form = this.formBuilder.group({
-      name: [null, [Validators.required]]
-    });
-  }
-
-  onAdd() {
-    if (this.form.get("name").value) {
-      const add: CashflowCategory = {
-        name: this.form.get("name").value
-      };
-      this.addCategory(add);
-    }
-  }
 }

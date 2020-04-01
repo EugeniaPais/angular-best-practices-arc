f32a5f22619b5b6d78b8fba86f48e95125bc39bf
EugeniaPais <mariaeugeniapais@gmail.com>
2020-03-31 23:03:43 -0300

    refactor categories / category-list

--- src/app/settings/components/categories/categories.component.ts
+++ src/app/settings/components/categories/categories.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CashflowCategory } from "../../models/cashflow-category";
+import { SettingsFacade } from "../../settings.facade";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  selector: "app-categories",
+  templateUrl: "./categories.component.html",
+  styleUrls: ["./categories.component.css"]
 })
 export class CategoriesComponent implements OnInit {
+  isUpdating$: Observable<boolean>;
+  categories$: Observable<CashflowCategory[]>;
+  form: FormGroup;
+  showUpdate: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private settingsFacade: SettingsFacade,
+    private formBuilder: FormBuilder
+  ) {
+    this.isUpdating$ = settingsFacade.isUpdating$();
   }
 
+  ngOnInit() {
+    this.initForm();
+    this.isUpdating$.subscribe(val => {
+      this.showUpdate = val;
+    });
+
+    this.settingsFacade.loadCashflowCategories().subscribe(val => {
+      this.categories$ = this.settingsFacade.getCashflowCategories$();
+    });
+  }
+
+  addCategory(category: CashflowCategory) {
+    this.settingsFacade.addCashflowCategory(category);
+  }
+
+  updateCategory(category: CashflowCategory) {
+    this.settingsFacade.updateCashflowCategory(category);
+  }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required]]
+    });
+  }
+
+  onAdd() {
+    if (this.form.get("name").value) {
+      const add: CashflowCategory = {
+        name: this.form.get("name").value
+      };
+      this.addCategory(add);
+    }
+  }
 }
--- src/app/settings/components/category-list/category-list.component.ts
+++ src/app/settings/components/category-list/category-list.component.ts
@@ -10,50 +10,8 @@ import { Observable } from "rxjs";
   styleUrls: ["./category-list.component.css"]
 })
 export class CategoryListComponent implements OnInit {
-  @Input() cashflowCategories$: CashflowCategory[];
-  newCategory: CashflowCategory = new CashflowCategory();
-  isUpdating$: Observable<boolean>;
-  showUpdate: boolean;
-  categories$: Observable<CashflowCategory[]>;
-  form: FormGroup;
-  constructor(
-    private settingsFacade: SettingsFacade,
-    private formBuilder: FormBuilder
-  ) {
-    this.isUpdating$ = settingsFacade.isUpdating$();
-  }
+  @Input() categories$: Observable<CashflowCategory[]>;
+  constructor() {}
 
-  ngOnInit() {
-    this.initForm();
-    this.isUpdating$.subscribe(val => {
-      this.showUpdate = val;
-    });
-
-    this.settingsFacade.loadCashflowCategories().subscribe(val => {
-      this.categories$ = this.settingsFacade.getCashflowCategories$();
-    });
-  }
-
-  addCategory(category: CashflowCategory) {
-    this.settingsFacade.addCashflowCategory(category);
-  }
-
-  updateCategory(category: CashflowCategory) {
-    this.settingsFacade.updateCashflowCategory(category);
-  }
-
-  initForm() {
-    this.form = this.formBuilder.group({
-      name: [null, [Validators.required]]
-    });
-  }
-
-  onAdd() {
-    if (this.form.get("name").value) {
-      const add: CashflowCategory = {
-        name: this.form.get("name").value
-      };
-      this.addCategory(add);
-    }
-  }
+  ngOnInit() {}
 }

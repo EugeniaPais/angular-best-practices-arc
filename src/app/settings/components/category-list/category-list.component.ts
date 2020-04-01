e017a484787a3b6816c3e9ba4c1206e23888686b
EugeniaPais <mariaeugeniapais@gmail.com>
2020-03-31 23:08:27 -0300

    Adding change detection strategy

--- src/app/settings/components/category-list/category-list.component.ts
+++ src/app/settings/components/category-list/category-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy
+} from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { CashflowCategory } from "../../models/cashflow-category";
 import { SettingsFacade } from "../../settings.facade";
@@ -7,7 +12,8 @@ import { Observable } from "rxjs";
 @Component({
   selector: "app-category-list",
   templateUrl: "./category-list.component.html",
-  styleUrls: ["./category-list.component.css"]
+  styleUrls: ["./category-list.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoryListComponent implements OnInit {
   @Input() categories$: Observable<CashflowCategory[]>;
